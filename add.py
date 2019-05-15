0f4ae721ebac86a23ca2dfb8835e964e6ad1eec2
BRINDA RAJESH <19917@fisat>
2019-05-15 04:54:03 -0400

    Create a function to add 4 numbers

--- add.py
+++ add.py
@@ -4,6 +4,10 @@ def add2(num1,num2):
 def add3(num1,num2,num3):
     sum=num1+num2=num3
     return s
+def add4(num1,num2,num3,num4):
+    sum=num1+num2+num3+num4
+    return s
 
 print add2(2,3)
 print add3(3,4,5)
+print add4(4,5,6)
